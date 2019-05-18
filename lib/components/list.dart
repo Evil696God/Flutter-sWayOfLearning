8dfa1e91bd7322250a0d2e675079e8d14b2f6c81
wangchenxing <165132359@qq.com>
2019-05-18 16:39:19 +0800

    【代码】 添加toast依赖库;列表添加点击事件;点击事件弹出toast

--- lib/components/list.dart
+++ lib/components/list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class List extends StatefulWidget {
   @override
@@ -13,12 +14,13 @@ class ListState extends State<List> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
     return new ListView.builder(
         itemCount: 10,
         itemBuilder: (BuildContext context, int index) {
           return new Card(
             child: new Container(
-              padding: new EdgeInsets.all(10.0),
+              padding: new EdgeInsets.all(5.0),
               child: new ListTile(
                 subtitle: new Container(
                   child: new Column(
@@ -63,6 +65,17 @@ class ListState extends State<List> {
                   Icons.keyboard_arrow_right,
                   color: Colors.grey,
                 ),
+                onTap: () {
+                  Fluttertoast.showToast(
+                      msg: "网络连接错误",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                      timeInSecForIos: 100,
+                      backgroundColor: Color(0xff9E9E9E),
+                      textColor: Color(0xffffffff),
+                      fontSize: 16
+                  );
+                },
               ),
             ),
           );
